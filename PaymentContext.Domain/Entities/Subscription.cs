e33872d1bb5d120996fe9f39f0e519e1378c09bd
Joao Romeiro <j.romeiro@live.com>
2021-02-07 10:51:54 -0300

    Corrupcao no Codigo

--- PaymentContext.Domain/Entities/Subscription.cs
+++ PaymentContext.Domain/Entities/Subscription.cs
@@ -1,14 +1,42 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PaymentContext.Domain.Entities
 {
     public class Subscription
     {
-        public DateTime CreateDate { get; set; }
-        public DateTime LastUpdateDate { get; set; }
-        public DateTime? ExpireDate { get; set; }
-        public bool Active { get; set; }
-        public List<Payment> Payments { get; set; }
+        private IList<Payment> _payments;
+        public Subscription(DateTime? expireDate)
+        {
+            this.CreateDate = DateTime.Now;
+            this.LastUpdateDate = DateTime.Now;
+            this.ExpireDate = expireDate;
+            this.Active = true;
+            this._payments = new List<Payment>();
+        }
+
+        public DateTime CreateDate { get; private set; }
+        public DateTime LastUpdateDate { get; private set; }
+        public DateTime? ExpireDate { get; private set; }
+        public bool Active { get; private set; }
+        public IReadOnlyCollection<Payment> Payments { get { return this._payments.ToArray(); } }
+
+        public void AddPayment(Payment payment)
+        {
+            this._payments.Add(payment);
+        }
+
+        public void Activate()
+        {
+            this.Active = true;
+            this.LastUpdateDate = DateTime.Now;
+        }
+
+        public void Inactivate()
+        {
+            this.Active = false;
+            this.LastUpdateDate = DateTime.Now;
+        }
     }
 }
---
+++ PaymentContext.Tests/Entities/StudentsTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void AdcionarAssinatura()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student("Joao", "Romeiro", "326915400", "j.romeiro@live.com");
+            student.AddSubscription(subscription);
+        }
+    }
+}
